b657576a4b6794b2fc2e99fda7d409fe60a43116
Magnus Kühn <magnus.khn@gmail.com>
2022-10-20 11:09:15 +0200

    Automatically copy short URL into the clipboard

--- templates/urlSaved.php
+++ templates/urlSaved.php
@@ -1,2 +1,5 @@
-<p class="alert alert-success container">Ihre &Auml;nderungen wurden gespeichert. Verwenden Sie die kurze URL
-	<a href="<?php echo $parameter; ?>" target="blank"><?php echo $parameter; ?></a>.</p>
+<p class="alert alert-success container">Ihre &Auml;nderungen wurden gespeichert. Die kurze URL
+	<a href="<?php echo $parameter; ?>" target="blank"><?php echo $parameter; ?></a> wurde in die Zwischenablage kopiert.</p>
+<script>
+	navigator.clipboard.writeText('<?php echo $parameter; ?>');
+</script>
